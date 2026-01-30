14b84af1323f4f2c67bc84ad8192b69bf7fc46ea
SIONMATHEW <sayujion@gmail.com>
2026-01-30 13:01:52 +0800

    Level 1:- Echo

--- src/main/java/KingSIM.java
+++ src/main/java/KingSIM.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 public class KingSIM {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm KingSIM");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner ed= new Scanner(System.in);
+        while(true) {
+            String x = ed.nextLine();
+            String y = x.trim().toLowerCase();
+            if (y.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(x);
+            System.out.println("____________________________________________________________");
+        }
+        ed.close();
     }
 }
