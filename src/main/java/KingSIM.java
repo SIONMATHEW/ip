1dcd2e9521e6cba94cae3c6fdf781754b1ccf32d
SIONMATHEW <sayujion@gmail.com>
2026-01-30 13:22:48 +0800

    Level-3: Mark and unmark tasks

--- src/main/java/KingSIM.java
+++ src/main/java/KingSIM.java
@@ -1,36 +1,64 @@
 import java.util.Scanner;
+
 public class KingSIM {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        final String LINE = "____________________________________________________________";
+        System.out.println(LINE);
         System.out.println("Hello! I'm KingSIM");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        Scanner ed= new Scanner(System.in);
-        String[] list= new String[100];
-        int tasks= 0;
-        while(true) {
-            String x = ed.nextLine();
-            String y = x.trim().toLowerCase();
-            if (y.equals("bye")) {
-                System.out.println("____________________________________________________________");
+        System.out.println(LINE);
+        Scanner ed = new Scanner(System.in);
+
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
+
+        while (true) {
+            String input = ed.nextLine();
+            String command = input.trim();
+
+            if (command.equalsIgnoreCase("bye")) {
+                System.out.println(LINE);
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                System.out.println(LINE);
                 break;
             }
-            if(y.equals("list")){
-                System.out.println("____________________________________________________________");
-                for(int i=0;i<tasks;i++)
-                {
-                    System.out.println((i+1) + ". " +list[i]);
+
+            if (command.equalsIgnoreCase("list")) {
+                System.out.println(LINE);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
-                System.out.println("____________________________________________________________");
+                System.out.println(LINE);
                 continue;
             }
-            list[tasks++] = x;
-            System.out.println("____________________________________________________________");
-            System.out.println("added: " + x);
-            System.out.println("____________________________________________________________");
 
+            if (command.toLowerCase().startsWith("mark ")) {
+                int index = Integer.parseInt(command.substring(5).trim()) - 1;
+                tasks[index].markDone();
+
+                System.out.println(LINE);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[index]);
+                System.out.println(LINE);
+                continue;
+            }
+
+            if (command.toLowerCase().startsWith("unmark ")) {
+                int index = Integer.parseInt(command.substring(7).trim()) - 1;
+                tasks[index].unmarkDone();
+
+                System.out.println(LINE);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[index]);
+                System.out.println(LINE);
+                continue;
+            }
+
+            tasks[taskCount++] = new Task(command);
+            System.out.println(LINE);
+            System.out.println("added: " + command);
+            System.out.println(LINE);
         }
         ed.close();
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task{
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void unmarkDone() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
