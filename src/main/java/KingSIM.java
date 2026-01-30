89b37272c252926ea0b52ebf56a0fc6b18a6cf91
SIONMATHEW <sayujion@gmail.com>
2026-01-30 13:12:00 +0800

    Level-2: Add and list tasks

--- src/main/java/KingSIM.java
+++ src/main/java/KingSIM.java
@@ -6,6 +6,8 @@ public class KingSIM {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
         Scanner ed= new Scanner(System.in);
+        String[] list= new String[100];
+        int tasks= 0;
         while(true) {
             String x = ed.nextLine();
             String y = x.trim().toLowerCase();
@@ -15,9 +17,20 @@ public class KingSIM {
                 System.out.println("____________________________________________________________");
                 break;
             }
+            if(y.equals("list")){
+                System.out.println("____________________________________________________________");
+                for(int i=0;i<tasks;i++)
+                {
+                    System.out.println((i+1) + ". " +list[i]);
+                }
+                System.out.println("____________________________________________________________");
+                continue;
+            }
+            list[tasks++] = x;
             System.out.println("____________________________________________________________");
-            System.out.println(x);
+            System.out.println("added: " + x);
             System.out.println("____________________________________________________________");
+
         }
         ed.close();
     }
